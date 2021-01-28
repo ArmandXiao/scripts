83772d0326257966e00132977b4433158bd6d137
ArmandXiao <xiao_lingao@outlook.com>
2021-01-28 21:05:20 +0800

    update dwm status bar

--- autostart.sh
+++ autostart.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-/bin/bash ~/scripts/dwm-status.sh &
-/bin/bash ~/scripts/tap-to-click.sh &
-/bin/bash ~/scripts/inverse-scroll.sh &
-/bin/bash ~/scripts/wp-autochange.sh &
+#/bin/bash ~/scripts/dwm-status.sh &
+#/bin/bash ~/scripts/tap-to-click.sh &
+# /bin/bash ~/scripts/inverse-scroll.sh &
+#/bin/bash ~/scripts/wp-autochange.sh &
 #picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
-picom -b
-nm-applet &
-xfce4-power-manager &
+picom -b &
+feh --bg-scale ~/.config/wallpaper/Japan-4k-2.jpg &
+# nm-applet &
+# xfce4-power-manager &
 #kdeconnect-indicator &
-fcitx5 &
+fcitx &
 #xfce4-volumed-pulse &
-/bin/bash ~/scripts/run-mailsync.sh &
-/bin/bash ~/scripts/setxmodmap-colemak.sh &
-~/scripts/autostart_wait.sh &
+# /bin/bash ~/scripts/run-mailsync.sh &
+#/bin/bash ~/scripts/setxmodmap-colemak.sh &
+#~/scripts/autostart_wait.sh &
+
+# https://github.com/thytom/dwmbar
+dwmbar &
