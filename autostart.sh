d0aeeeed797e7a6e8ad007138cc49773b269e88a
ArmandXiao <xiao_lingao@outlook.com>
2021-02-23 20:12:36 +0800

    change and add scripts

--- autostart.sh
+++ autostart.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
+# enable system volume buttons
+start-pulseaudio-x11 &
+
 #/bin/bash ~/scripts/dwm-status.sh &
 #/bin/bash ~/scripts/tap-to-click.sh &
 # /bin/bash ~/scripts/inverse-scroll.sh &
-#/bin/bash ~/scripts/wp-autochange.sh &
 #picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
 picom -b &
-feh --bg-scale ~/.config/wallpaper/Japan-4k-2.jpg &
+/bin/bash ~/scripts/wp-change.sh &
+
+# change key map
+setxkbmap -option ctrl:nocaps
+# /bin/bash ~/scripts/swap-caps-backspace.sh &
+
 # nm-applet &
 # xfce4-power-manager &
-#kdeconnect-indicator &
-fcitx &
-#xfce4-volumed-pulse &
+flameshot &
+fcitx5 &
 # /bin/bash ~/scripts/run-mailsync.sh &
-#/bin/bash ~/scripts/setxmodmap-colemak.sh &
 #~/scripts/autostart_wait.sh &
 
 # https://github.com/thytom/dwmbar
 dwmbar &
+
+insync start &
